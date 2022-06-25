b585a6dff7c01df74a1a4c78a961c6cdbea3c310
QZLin <qzlin01@163.com>
2022-06-25 15:45:26 +0800

    test-873706

--- check_if_new.ps1
+++ check_if_new.ps1
@@ -2,10 +2,11 @@ Push-Location DeviceFarmer_minicap
 $json = (ConvertFrom-Json $env:last_release)
 
 $current_commit = git log -1 --format=%h
-
+Write-Output "::echo::on"
 if (-not $json.tag_name -eq $current_commit) {
     Write-Output "::set-output name=new_commit::true"
-}else{
+}
+else {
     Write-Output "::set-output name=new_commit::false"
 }
 Pop-Location
