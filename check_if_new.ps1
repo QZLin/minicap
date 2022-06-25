d492f77330a11c65bc36bcfac126f7783f874b78
QZLin <qzlin01@163.com>
2022-06-25 15:59:08 +0800

    test-579377

--- check_if_new.ps1
+++ check_if_new.ps1
@@ -4,7 +4,7 @@ $json = (ConvertFrom-Json $env:last_release)
 $current_commit = git log -1 --format=%h
 Write-Output "::echo::on"
 Write-Output "$current_commit - $($json.tag_name)"
-if (-not $json.tag_name -eq $current_commit) {
+if (-not ($json.tag_name -eq $current_commit)) {
     Write-Output "::save-state name=new_commit::true"
 }
 else {
