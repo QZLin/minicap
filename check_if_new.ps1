718ee2e006b68bcf38701d740bb5f2cde4cb65fa
QZLin <qzlin01@163.com>
2022-06-25 15:48:01 +0800

    test-886077

--- check_if_new.ps1
+++ check_if_new.ps1
@@ -4,9 +4,9 @@ $json = (ConvertFrom-Json $env:last_release)
 $current_commit = git log -1 --format=%h
 Write-Output "::echo::on"
 if (-not $json.tag_name -eq $current_commit) {
-    Write-Output "::set-output name=new_commit::true"
+    Write-Output "::save-state name=new_commit::true"
 }
 else {
-    Write-Output "::set-output name=new_commit::false"
+    Write-Output "::save-state name=new_commit::false"
 }
 Pop-Location
