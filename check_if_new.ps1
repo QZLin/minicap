0499767c36993f147d922e919c9b7f271cc3268d
QZLin <qzlin01@163.com>
2022-06-25 17:22:05 +0800

    test-619367

--- check_if_new.ps1
+++ check_if_new.ps1
@@ -6,14 +6,14 @@ $current_commit = git log -1 --format=%h
 $current_commit_msg = git log -1 --format=%s
 Write-Output "::echo::on"
 Write-Output "current:$current_commit - last:$($json.tag_name)"
-if (-not ($json.tag_name -eq $current_commit)) {
-    $Env:new_commit = "true"
-}
-else {
-    $Env:new_commit = "false"
-}
-Write-Output "new_commit=$($Env:new_commit)" >> $Env:GITHUB_ENV
-Write-Output "new_commit=$($Env:new_commit)"
-Write-Output "current_commit_msg=$($Env:current_commit_msg)" >> $Env:GITHUB_ENV
-Write-Output "current_commit_msg=$($Env:current_commit_msg)"
+
+if (-not ($json.tag_name -eq $current_commit)) { $new_commit = "true" } else { $new_commit = "false" }
+
+Write-Output "new_commit=$new_commit" >> $Env:GITHUB_ENV
+Write-Output "new_commit=$new_commit"
+Write-Output "current_commit=$current_commit" >> $Env:GITHUB_ENV
+Write-Output "current_commit=$current_commit"
+Write-Output "current_commit_msg=$current_commit_msg" >> $Env:GITHUB_ENV
+Write-Output "current_commit_msg=$current_commit_msg"
+
 Pop-Location
