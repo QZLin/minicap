d43a8df6b1211fc23017c9226c2b4952880b8147
QZLin <qzlin01@163.com>
2022-06-25 17:14:42 +0800

    test-394421

--- check_if_new.ps1
+++ check_if_new.ps1
@@ -1,7 +1,9 @@
 Push-Location DeviceFarmer_minicap
 $json = (ConvertFrom-Json $env:last_release)
+# $json.name; $json.tag_name
 
 $current_commit = git log -1 --format=%h
+$current_commit_msg = git log -1 --format=%s
 Write-Output "::echo::on"
 Write-Output "current:$current_commit - last:$($json.tag_name)"
 if (-not ($json.tag_name -eq $current_commit)) {
@@ -12,4 +14,6 @@ else {
 }
 Write-Output "new_commit=$($Env:new_commit)" >> $Env:GITHUB_ENV
 Write-Output "new_commit=$($Env:new_commit)"
+Write-Output "current_commit_msg=$($Env:current_commit_msg)" >> $Env:GITHUB_ENV
+Write-Output "current_commit_msg=$($Env:current_commit_msg)"
 Pop-Location
